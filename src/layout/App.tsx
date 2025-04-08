61c43bb42a60dc838a0b28aa1343f863cdd77076
Mahin <mahindipto18@gmail.com>
2025-04-08 15:40:00 +0600

    added Navbar

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/ui/shared/Navbar";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
