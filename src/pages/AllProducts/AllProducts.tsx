2118c6dc992464e937d6b348d1ab447a4b3e77b0
meheraz1100 <meheraz1100@gmail.com>
2025-04-11 03:00:53 +0600

    all products show in UI

--- src/pages/AllProducts/AllProducts.tsx
+++ src/pages/AllProducts/AllProducts.tsx
@@ -1,20 +1,50 @@
 import { useGetCarsQuery } from "@/redux/features/carPost/carApi";
 
 const AllProducts = () => {
-  // Destructure the result from the query hook
-  const {  data: allCarsData, isLoading,  refetch } = useGetCarsQuery([]);
+  const { data: allCarsData, isLoading, refetch } = useGetCarsQuery([]);
 
-  console.log(allCarsData);
   if (isLoading) return <div>Loading...</div>;
-
-  // if (error) return <div>Error: {error.message}</div>;
-  // refetch();
+  if (!allCarsData?.data) return <div>No cars found.</div>;
 
   return (
     <div>
-      AllProducts {allCarsData.data?.length}
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+        {allCarsData.data.map((car, index) => (
+          <div
+            key={index}
+            className="mb-4 flex flex-col rounded-lg bg-white p-4 shadow-lg"
+          >
+            <img
+              src={car.image}
+              alt={car.name}
+              className="h-full w-full rounded-lg"
+            />
+            <div className="mt-3 md:ml-4">
+              <h3 className="text-xl font-bold">
+                Model: <span className="text-purple-700">{car.name}</span>
+              </h3>
+              <p className="mt-2 text-xl font-semibold text-gray-900">
+                Price: ${car.price}
+              </p>
+              <div className="my-2 flex flex-col gap-2 md:flex-row md:gap-5">
+                <p className="text-lg font-semibold text-gray-600">
+                  Category:{" "}
+                  <span className="rounded-lg border-2 border-purple-700 p-1 text-purple-500">
+                    {car.category}
+                  </span>
+                </p>
+                <p className="text-lg font-semibold text-gray-600">
+                  Brand: <span className="text-purple-700">{car.brand}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
 
 export default AllProducts;
