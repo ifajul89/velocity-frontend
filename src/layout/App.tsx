71af1e1fa7143714f0102fd4965c5f26df8dcbba
meheraz1100 <meheraz1100@gmail.com>
2025-04-13 01:37:55 +0600

    fix footer

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/ui/shared/Footer";
 import Navbar from "@/components/ui/shared/Navbar";
 import { Outlet } from "react-router-dom";
 
@@ -8,6 +9,7 @@ const App = () => {
       <div className="mt-12 md:mt-14 lg:mt-16">
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 };
